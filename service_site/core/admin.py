c74915d8f3d135e5d6dc77b5517247bdca6cc886
Amir Mohammad Talebi <talebiamirmohammad78@gmail.com>
2026-06-25 07:04:23 +0330

    Add career and job application models

--- service_site/core/admin.py
+++ service_site/core/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import Service, Portfolio, Order
 from .models import *
 
-admin.site.register(Service)
-admin.site.register(Portfolio)
-admin.site.register(Order)
 admin.site.register(Contact)
 admin.site.register(Testimonial)
 admin.site.register(Career)
